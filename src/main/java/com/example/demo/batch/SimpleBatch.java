9f756e835b87aedc7be548ac73da81d759769d2c
leeuigang <leeuigang@epopcon.com>
2023-03-18 15:13:52 +0900

    multiThread example 실습

--- src/main/java/com/example/demo/batch/SimpleBatch.java
+++ src/main/java/com/example/demo/batch/SimpleBatch.java
@@ -5,10 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,41 +30,43 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class SimpleBatch {
 
-    private int poolSize = 4;
+    private int poolSize = 10;
 
     @Bean
-    public Job simpleJob(JobRepository jobRepository, Step step){
+    public Job simpleJob(JobRepository jobRepository, Step step) {
         return new JobBuilder("simpleJob", jobRepository)
                 .start(step)
                 .build();
     }
 
     @Bean
-    public Step simpleStep(JobRepository jobRepository, PlatformTransactionManager transactionManager){
+    @JobScope
+    public Step simpleStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("simpleStep", jobRepository)
                 .tasklet(simpleTasklet(), transactionManager)
+                .throttleLimit(poolSize - 1)            //기본값이 5개인것같은데 어떻게 바꾸는지 모르겠다. 이걸 사용하면 원하는 횟수만큼 실행시킬순있다.
                 .taskExecutor(executor())
                 .build();
     }
 
     @Bean
-    public Tasklet simpleTasklet(){
+    @StepScope
+    public Tasklet simpleTasklet() {
         return (stepContribution, chunkContext) -> {
-            for(int i = 0; i < 10; i++){
-                System.out.println(i);
-                Thread.sleep(1);
-            }
+            log.info("{}",Thread.currentThread().getName());
             return RepeatStatus.FINISHED;
         };
     }
 
     @Bean
+    @JobScope
     public TaskExecutor executor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor(); // (2)
         executor.setCorePoolSize(poolSize);
         executor.setMaxPoolSize(poolSize);
         executor.setThreadNamePrefix("multi-thread-");
         executor.setWaitForTasksToCompleteOnShutdown(Boolean.TRUE);
+        executor.setAllowCoreThreadTimeOut(true);
         executor.initialize();
         return executor;
     }
